4808021cf62f562b45128addc9dcc40401ab1930
tovBender <bend.r.bender@gmail.com>
2026-03-03 13:33:13 +0300

    update doc

--- dist/js/script.js
+++ dist/js/script.js
@@ -856,12 +856,9 @@ var DSFUI = (function (exports) {
       });
       $('.selectpicker').selectpicker();
       $('.js-copy-to-clipboard').click(function (e) {
-        var copyText = this.getAttribute('data-copy');
-        navigator.clipboard.writeText(copyText).then(function () {
-          console.log('Async: Copying to clipboard was successful!');
-        });
         this.classList.add('text-success');
-        $('.toast').toast('show');
+        setTimeout(this.classList.remove('text-success'), 3000);
+        $('#liveToast2').toast('show');
       });
     }
 
