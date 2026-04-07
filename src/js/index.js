ac4e84bd64e8a4230c87bce85e360811e6c31459
tovBender <bend.r.bender@gmail.com>
2026-04-07 17:32:50 +0300

    update ui v1.6.137

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+export { AutoresizeTextarea } from './autoresizeTextarea.js';
+export { CollapseParent } from './CollapseParent.js';
+export { initCopyDataAttrToClipboardBtns } from './copyTextToClipboardNotify.js';
+export { CustomFileUpload } from './CustomFileUpload.js';
+export { FormsFree } from './forms-free.js';
+export { ParseHeight } from './parseHeight.js';
